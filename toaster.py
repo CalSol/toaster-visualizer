d3e575122ff024667164252b70fde99ead20184b
Richard Lin <elpato25@gmail.com>
2019-03-25 12:52:33 -0700

    implementation

--- toaster.py
+++ toaster.py
@@ -1,5 +1,7 @@
 import datetime
 import serial
+import csv
+import re
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,9 +10,11 @@ SERIAL_PATH = 'COM6'
 SERIAL_BAUD = 115200
 
 ELEMENT_COUNT = 8
-SAMPLES = 60 * 60
+WINDOW = 60 * 60
 
 OUT_PREFIX = "toaster_"
+
+TEXT_ALPHA = 0.75
 
 TEST_LINE = "0: 73 2: 79 3: 80 4: 75 5: 81 6: 80 7: 82"
 
@@ -18,23 +22,78 @@ if __name__ == "__main__":
   start_time = datetime.datetime.now()
   out_filename = OUT_PREFIX + start_time.strftime("%Y%m%d_%H%M%S") + ".csv"
 
-  with serial.Serial(SERIAL_PATH, SERIAL_BAUD) as ser, open(out_filename, "w") as csv_out:
-    # initialize plot window and static elements
-    fig, ax = plt.subplots(2, 1, constrained_layout=True)
+  with serial.Serial(SERIAL_PATH, SERIAL_BAUD) as ser, open(out_filename, 'w', newline='') as csv_out:
+  # with open(out_filename, 'w', newline='') as csv_out:
+    csvwriter = csv.writer(csv_out)
+    csvwriter.writerow(['timestamp'] + list(range(0, ELEMENT_COUNT)))
 
-    plots = map(lambda n: ax[0].plot([]),range(0, ELEMENT_COUNT))
-    avg_line = ax[0].axhline(0)
-    min_text = ax[0].text(0, 0, "")
-    max_text = ax[0].text(0, 0, "")
-    avg_text = ax[0].text(0, 0, "")
-    ax[0].set_xlabel("time")
-    ax[0].set_ylabel("temp")
+    # initialize plot window and static elements
+    fig, ax = plt.subplots(1, 1, constrained_layout=True)
+
+    plots = list(map(lambda n: ax.plot([])[0], range(0, ELEMENT_COUNT)))
+    print(plots)
+    avg_line = ax.axhline(0)
+    min_text = ax.text(0, 0, "", ha='right', va='top', alpha=TEXT_ALPHA)
+    max_text = ax.text(0, 0, "", ha='right', va='bottom', alpha=TEXT_ALPHA)
+    avg_text = ax.text(0, 0, "", ha='right', va='center', alpha=TEXT_ALPHA)
+    ax.set_xlabel("time")
+    ax.set_ylabel("temp")
 
     plt.ion()
     plt.draw()
 
     while True:
       line = ser.readline()
+      # line = TEST_LINE
       line_time = datetime.datetime.now()
+      delta_time = line_time.timestamp() - start_time.timestamp()
       print("[%s] received: %s" % (line_time.strftime("%H:%M:%S"), line))
 
+      match = re.findall(r'(\d+):\s*(\d+\.?\d*)', line)
+      match = list(map(lambda x: (int(x[0]), float(x[1])), match))
+
+      if match:
+        print("Decoded: %s" % (match))
+
+        csvrow = list(map(lambda id: '', range(0, ELEMENT_COUNT)))
+        for (id, val) in match:
+          csvrow[id] = str(val)
+        csvwriter.writerow([line_time.timestamp()] + csvrow)
+        csv_out.flush()
+
+        for (id, val) in match:
+          if id >= ELEMENT_COUNT:
+            print("(%s, %.1f) id out of range" % (id, val))
+          else:
+            plot = plots[id]
+            plot.set_label("%s: %.1f" % (id,  val))
+            xdata = plot.get_xdata()
+            ydata = plot.get_ydata()
+            while xdata.any() and xdata[0] < delta_time - WINDOW:
+              xdata = xdata[1:]
+              ydata = ydata[1:]
+            xdata = np.append(xdata, delta_time)
+            ydata = np.append(ydata, val)
+            plot.set_xdata(xdata)
+            plot.set_ydata(ydata)
+
+        match_rev = list(map(lambda x: (x[1], x[0]), match))
+        match_rev.sort()
+        match_vals = list(map(lambda x: x[1], match))
+        avg = sum(match_vals)/len(match_vals)
+        min_text.set_x(delta_time)
+        max_text.set_x(delta_time)
+        avg_text.set_x(delta_time)
+        min_text.set_y(match_rev[0][0])
+        max_text.set_y(match_rev[-1][0])
+        avg_text.set_y(avg)
+        min_text.set_text("min %s: %.1f" % (match_rev[0][1], match_rev[0][0]))
+        max_text.set_text("max %s: %.1f" % (match_rev[-1][1], match_rev[-1][0]))
+        avg_text.set_text("avg(%s): %.1f, spd: %.1f" % (len(match_vals), avg, match_vals[-1] - match_vals[0]))
+
+        ax.legend(loc=2)  # upper left
+        ax.set_xlim(delta_time - WINDOW,  delta_time)
+        ax.set_ylim(0, 100)
+
+        plt.draw()
+        plt.pause(1)
